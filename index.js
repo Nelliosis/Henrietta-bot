c295caaf6029e9d08739fb795c11b8ec9a914041
Junel Capawing <jcapawing@gmail.com>
2022-03-29 00:46:17 +0800

    added intents and first command

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const { Client, Intents, User } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,                   // server administrative functions
+        Intents.FLAGS.GUILD_MESSAGES,           // allows to send and receive messages
+        Intents.FLAGS.GUILD_VOICE_STATES,       // allows voice channel entry
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,  // allows reactions
+        Intents.FLAGS.DIRECT_MESSAGES           // allows bot to DM users
+    ]
+});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log(`Henrietta Huckleberry, as needed, commanding: ${client.user.tag}`);
 });
 
-// Login to Discord with your client's token
+client.on("messageCreate", (message) => {
+    if (message.content == "Hi Hetty") {
+        message.reply(`Hello, ${message.author.username}`)
+    }
+})
+
+// Login to Discord
 client.login(token);
