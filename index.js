b75d0610146abf187f458d39a52909e0609e832e
Junel Capawing <jcapawing@gmail.com>
2022-03-29 22:04:07 +0800

    refactored code and added commands

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
-const { Client, Intents, User } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -13,14 +14,48 @@ const client = new Client({
     ]
 });
 
-// When the client is ready, run this code (only once)
+//prepare commands directory to be read
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//read commands directory
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run
 client.once('ready', () => {
-    console.log(`Henrietta Huckleberry, as needed, commanding: ${client.user.tag}`);
+    console.log('Welcome, Henrietta Huckleberry.');
+    console.log(`Connected to the Berry Network as: ${client.user.tag}`);
+
 });
 
+// call commands on interaction
+client.on('interactionCreate', async interaction => {
+    // if not a command, ignore
+    if (!interaction.isCommand()) return;
+
+    //collect commands from the collection
+    const command = client.commands.get(interaction.commandName);
+
+    // if not a command, ignore
+    if (!command) return;
+
+    // execute if a command, catch e if not
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Berry Network: Error during execution', ephemeral: true });
+    }
+});
+
+
+// Hello message
 client.on("messageCreate", (message) => {
     if (message.content == "Hi Hetty") {
-        message.reply(`Hello, ${message.author.username}`)
+        message.reply(`Hello, <@${message.author.id}>`)
     }
 })
 
