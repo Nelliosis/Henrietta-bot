6771b2539f7f559e25e33525c5e621b5883b14c5
Junel Capawing <jcapawing@gmail.com>
2022-03-30 18:32:25 +0800

    Changed to global commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,6 +13,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Berry Network: Commands registered.'))
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+    .then(() => console.log('[BERRY NOTE]: Commands registered.'))
     .catch(console.error);
