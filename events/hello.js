ed2906ad6a944cf43baafaa55a88c26d4c9eeca4
Junel Capawing <jcapawing@gmail.com>
2022-04-01 01:33:05 +0800

    changed return to await

--- events/hello.js
+++ events/hello.js
@@ -1,8 +1,9 @@
 module.exports = {
     name: "messageCreate",
     async execute(message) {
+        // change string to lowercase and reply
         if (message.content.toLowerCase() == "hi hetty") {
-            return message.reply(`Hello, <@${message.author.id}>`);
+            await message.reply(`Hello, <@${message.author.id}>`);
         }
     },
 };
