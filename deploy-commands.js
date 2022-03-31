d737e4c6ba739da6e8e654777872bef87b69766b
Junel Capawing <jcapawing@gmail.com>
2022-04-01 01:21:39 +0800

    added comments

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,14 @@
+// external libraries
 const fs = require('node:fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, token } = require('./config.json');
 
+// set up the commands array
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// read commands
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
@@ -13,6 +16,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
+// push the commands to client
 rest.put(Routes.applicationCommands(clientId), { body: commands })
     .then(() => console.log('[BERRY NOTE]: Commands registered.'))
     .catch(console.error);
