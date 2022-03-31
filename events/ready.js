a17e906b85dc0a2e0da35a70036a7ea7c95aa1ce
Junel Capawing <jcapawing@gmail.com>
2022-04-01 01:33:33 +0800

    added comments

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "ready",
     once: "true",
     execute(client) {
+        // when ready, initiate
         console.log('Welcome, Henrietta Huckleberry.');
         console.log(`[BERRY INIT]: Connected to the Berry Network as: ${client.user.tag}`);
     },
