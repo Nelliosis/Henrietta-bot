c413787ee9d32c4140c6c771457041f79c5b010b
Junel Capawing <jcapawing@gmail.com>
2022-04-08 15:55:43 +0800

    synced data modules entries

--- handlers/utilities/trackObjectifer.js
+++ handlers/utilities/trackObjectifer.js
@@ -1,18 +1,19 @@
 function trackObjectifier(track, platform, requester) {
     let data;
-    if (platform == 'sp') {
+    if (platform === 'sp') {
         data = {
             track: `${track.name} - ${track.artists[0].name}`,
             url: track.url,
-            platform,
+            user: requester,
+            platform: platform,
         };
     }
     else {
         data = {
             track: track.title,
             url: track.url,
-            requester,
-            platform,
+            user: requester,
+            platform: platform,
         };
     }
     return data;
