8ea7f9a334ab3a7e30caae858109b7308025efd5
Junel Capawing <jcapawing@gmail.com>
2022-04-09 18:08:38 +0800

    added logs for skip

--- commands/skip.js
+++ commands/skip.js
@@ -7,11 +7,17 @@ module.exports = {
         .setDescription('Skip the current track'),
     async execute(interaction) {
 
+        // report to log
+        console.log(`[BERRY OPERATION] ${interaction.user.username} invoked /skip`);
+
         //get necessary data
         let connection = getVoiceConnection(interaction.guild.id);
         const player = connection.state.subscription.player; //copyright 28Goo
         
         player.stop();
+
+        // report to log
+        console.log('[BERRY OPERATION] Skip successful.');
 
         //todo embed
     }
