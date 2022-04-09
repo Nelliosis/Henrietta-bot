d89c72e8d10910a01588d46d6f1ca2a0117a3838
Junel Capawing <jcapawing@gmail.com>
2022-04-09 17:26:35 +0800

    added disconnect

---
+++ commands/disconnect.js
@@ -0,0 +1,33 @@
+// external libraries
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getVoiceConnection } = require('@discordjs/voice');
+
+//internal libraries
+const queueHandler = require('../handlers/queueSystem');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('disconnect')
+        .setDescription('exits the voice channel and destroys the queue.'),
+    async execute(interaction) {
+
+        // report to log
+        console.log(`[BERRY OPERATION] ${interaction.user.username} invoked /disconnect`);
+
+
+        //get necessary data
+        const guild = interaction.guild.id;
+        const connection = getVoiceConnection(guild);
+        const player = connection.state.subscription.player; //copyright 28Goo
+        
+        player.pause();
+        queueHandler.clearQueue(guild);
+        player.stop();
+        connection.destroy();
+
+        // report to log
+        console.log('[BERRY OPERATION] Disconnect successful.');
+
+        //todo embed
+    }
+};
