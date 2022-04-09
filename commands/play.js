4a0e979fe07e798b8a814f710d188a2d82237b5b
Junel Capawing <jcapawing@gmail.com>
2022-04-09 15:05:58 +0800

    refactored for better UX

--- commands/play.js
+++ commands/play.js
@@ -11,33 +11,18 @@ const queueHandler = require('../handlers/queueSystem');
 const objectifier = require('../handlers/utilities/trackObjectifer');
 const { startPlay } = require('../handlers/startPlay');
 
-module.exports = { 
+module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Commands Henrietta to play & queue music. Supports Spotify and YouTube.')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('url')
-                .setDescription('Loads a track from a URL')
-                .addStringOption(option => option.setName('url').setDescription('Input song URL here.').setRequired(true))
-)
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('playlist')
-                .setDescription('Loads tracks from a playlist')
-                .addStringOption(option => option.setName('playlist').setDescription('Input the playlist link here.').setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-                subcommand
-                    .setName('query')
-                    .setDescription('Loads a track by keywords')
-                    .addStringOption(option => option.setName('query').setDescription('Input your keywords here.').setRequired(true))
-        ),
-    
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('link, playlist or keywords.')
+                .setRequired(true)),
     async execute(interaction) {
 
         // check user connection to VC
-        if (!interaction.member.voice.channel) return interaction.reply("[BERRY COMMAND DENIED] Connect to a Voice Channel first.");
+        if (!interaction.member.voice.channel) return interaction.reply("Connect to a Voice Channel first.");
         
         //declare connection variables
         const guild = interaction.guild.id;
@@ -58,117 +43,67 @@ module.exports = {
         //refresh token
         if (play.is_expired()) await play.refreshToken();
 
-        //subcommand handler & add data to queue
-        if (interaction.options.getSubcommand() === "url") {
-            // Get input from option into a variable
-            const url = interaction.options.getString('url');
-            //report to log
-            console.log(`[BERRY OPERATION] ${user} invoked /play query by url with: ${url}`);
+        // Get input from option into a variable
+        const input = interaction.options.getString('input');
+        //report to log
+        console.log(`[BERRY OPERATION] ${user} invoked /play with: ${input}`);
 
-            //validate input
-            let check = await play.validate(url);
-            if (!check) {
-                //todo
-                //embed invalid url
-                return;
-            }
+        //validate input
+        let check = await play.validate(input);
+        if (!check) {
+            //todo
+            //embed invalid url
+            return;
+        }
 
-            switch (check) {
-                case 'yt_video':{
-                    //add song to queue
-                    const [track] = await play.search(url, { limit: 1 });
+        //once validated, commence enqueue by platform
+        switch (check) {
+            case 'search':
+            case 'yt_video':{
+                //add song to queue
+                const [track] = await play.search(input, { limit: 1 });
+                const trackData = objectifier.trackObjectifier(track, 'yt', user);
+                queueHandler.toQueue(guild, trackData);
+                //report to log
+                console.log("[BERRY NOTE] Successfully added to queue.");
+                //todo embed
+                break;}
+            case 'sp_track':{
+                //add song to queue
+                const track = await play.spotify(input, { limit: 1 });
+                const trackData = objectifier.trackObjectifier(track, 'sp', user);
+                queueHandler.toQueue(guild, trackData);
+                //report to log
+                console.log("[BERRY NOTE] Successfully added to queue.");
+                //todo embed
+                break;}
+            case 'yt_playlist': {
+                //add songs to queue
+                const songs = await play.playlist_info(input);
+                const tracks = await songs.all_videos();
+                for (const track of tracks) {
                     const trackData = objectifier.trackObjectifier(track, 'yt', user);
                     queueHandler.toQueue(guild, trackData);
-                    //report to log
-                    console.log("[BERRY NOTE] Successfully added to queue.");
-                    //todo embed
-                    break;}
-                case 'sp_track':{
-                    //add song to queue
-                    const [track] = await play.spotify(url, { limit: 1 });
+                }
+                //report to log
+                console.log("[BERRY NOTE] Successfully added to queue.");
+                //todo embed
+                break;}
+            case 'sp_playlist': {
+                //add songs to queue
+                const songs = await play.spotify(input);
+                const tracks = await songs.all_tracks();
+                for (const track of tracks) {
                     const trackData = objectifier.trackObjectifier(track, 'sp', user);
                     queueHandler.toQueue(guild, trackData);
-                    //report to log
-                    console.log("[BERRY NOTE] Successfully added to queue.");
-                    //todo embed
-                    break;}
-                case 'default':{
-                    //todo embed
-                    return;}
-            }
-
-        }
-        else if (interaction.options.getSubcommand() === "playlist") {
-            // Get input from option into a variable
-            const playlist = interaction.options.getString('playlist');
-            //report to log
-            console.log(`[BERRY OPERATION] ${user} invoked /play query by playlist with: ${playlist}`);
-
-            //validate input
-            let check = await play.validate(playlist);
-            if (!check) {
-                //todo
-                //embed invalid playlist
-                return;
-            }
-
-            switch (check) {
-                case 'yt_playlist': {
-                    //add songs to queue
-                    const songs = await play.playlist_info(playlist);
-                    const tracks = await songs.all_videos();
-                    for (const track of tracks) {
-                        const trackData = objectifier.trackObjectifier(track, 'yt', user);
-                        queueHandler.toQueue(guild, trackData);
-                    }
-                    //report to log
-                    console.log("[BERRY NOTE] Successfully added to queue.");
-                    //todo embed
-                    break;}
-                case 'sp_playlist': {
-                    //add songs to queue
-                    const songs = await play.spotify(playlist);
-                    const tracks = await songs.all_tracks();
-                    for (const track of tracks) {
-                        const trackData = objectifier.trackObjectifier(track, 'sp', user);
-                        queueHandler.toQueue(guild, trackData);
-                    }
-                    //report to log
-                    console.log("[BERRY NOTE] Successfully added to queue.");
-                    //todo embed
-                    break;}
-                case 'default':{
-                    //todo embed
-                    return;}
-            }
-        }
-        else if (interaction.options.getSubcommand() === "query") {
-            // Get input from option into a variable
-            const query = interaction.options.getString('query');
-            //report to log
-            console.log(`[BERRY OPERATION] ${user} invoked /play query by keywords with: ${query}`);
-
-            //validate input
-            let check = await play.validate(query);
-            if (!check) {
-                //todo
-                //embed invalid query
-                return;
-            }
-
-            switch (check) {
-                case 'search': {
-                    //add song to queue
-                    const [track] = await play.search(query, { limit: 1 });
-                    const trackData = objectifier.trackObjectifier(track, 'yt', user);
-                    queueHandler.toQueue(guild, trackData);
-                    //report to log
-                    console.log("[BERRY NOTE] Successfully added to queue.");
-                    //todo embed
-                    break;}
-                case 'default':{
-                    break;}
-            }
+                }
+                //report to log
+                console.log("[BERRY NOTE] Successfully added to queue.");
+                //todo embed
+                break;}
+            case 'default': {
+                //todo embed
+                break;}
         }
 
         // Check if bot is playing. 
@@ -177,7 +112,7 @@ module.exports = {
 		const subscription = connection.state.subscription;
 		if (subscription) {
 			const playerStatus = subscription.player.state.status;
-			if (playerStatus === 'playing') {
+            if (playerStatus === 'playing') {
 				return;
 			}
 		}
