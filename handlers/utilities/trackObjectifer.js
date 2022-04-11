d9ea217689fed30c1ed79731e0b4824138958401
Junel Capawing <jcapawing@gmail.com>
2022-04-12 01:33:31 +0800

    unified structure for sp and yt

--- handlers/utilities/trackObjectifer.js
+++ handlers/utilities/trackObjectifer.js
@@ -1,18 +1,31 @@
 function trackObjectifier(track, platform, requester) {
     let data;
     if (platform === 'sp') {
+        const minutes = Math.floor(track.durationInSec / 60);
+        const seconds = track.durationInSec % 60;
+        const duration = `${minutes}:${seconds}`;
+
         data = {
             track: `${track.name} - ${track.artists[0].name}`,
+            title: track.name,
+            artist: track.artists[0].name,
             url: track.url,
             user: requester,
+            thumbnail: track.thumbnail.url,
+            duration: duration,
             platform: platform,
         };
     }
     else {
+        const [thumb] = track.thumbnails; 
         data = {
             track: track.title,
+            title: track.title,
+            artist: track.channel.name,
             url: track.url,
             user: requester,
+            thumbnail: thumb.url,
+            duration: track.durationRaw,
             platform: platform,
         };
     }
