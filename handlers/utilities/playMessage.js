422f416fc13e9e777fb5a06e6a5a5b1750652dce
Junel Capawing <jcapawing@gmail.com>
2022-04-12 01:33:57 +0800

    added now playing after a track

---
+++ handlers/utilities/playMessage.js
@@ -0,0 +1,22 @@
+const { getVoiceConnection, AudioPlayerStatus } = require('@discordjs/voice');
+const { MessageEmbed } = require('discord.js');
+const embedder = require('./embedder');
+
+//playMessage module made by 28Goo/Cyntacs. All rights reserved.
+module.exports = {
+	playMessage: async (interaction, queueHead) => {
+		const embed = new MessageEmbed;
+		embedder.NowPlaying(embed, queueHead);
+		const msg = await interaction.channel.send({ embeds: [embed] });
+
+		const connection = getVoiceConnection(interaction.guild.id);
+		const player = connection.state.subscription.player;
+
+		player.on(AudioPlayerStatus.Idle, () => {
+			msg.delete()
+			.catch(error => {
+				if (error.code === 1008) console.error('Message Error: Message already deleted.');
+			});
+		});
+	},
+};
