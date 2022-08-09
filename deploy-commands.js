842f9d4e0f95b51806638ec94a97923820e05420
Nelliosis <jcapawing@gmail.com>
2022-08-09 21:58:39 +0800

    updated for djs v14

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord.js');
 const { clientId, token } = require('./config.json');
 
 // set up the commands array
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 // push the commands to client
 rest.put(Routes.applicationCommands(clientId), { body: commands })
